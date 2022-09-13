8a315587c56af9cbfafd7d3089f105d2af137d88
Adan Millard <adanmillard@gmail.com>
2022-09-13 14:28:01 +1200

    added mongo db docker-compose file linked up with backend api endpoints

---
+++ server/config/mongoConnection.js
@@ -0,0 +1,10 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = () => {
+//   mongoose
+//     .connect("mongodb://mongodb:27017/mongodb", { useNewUrlParser: true })
+//     .then(() => console.log("mongoDB Connected"))
+//     .catch((err) => console.log(err));
+// };
+
+// module.exports = connectDB;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,44 @@
-const express = require('express');
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
-})
+// app.get("/", (req, res) => {
+//   res.send("Express + TypeScript Server");
+// });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect("mongodb://mongodb:27017/mongodb", { useNewUrlParser: true })
+  .then(() => console.log("mongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const Item = require("./models/Laptops");
+
+app.get("/", (req, res) => {
+  Item.find()
+    .then((items) => res.send(items))
+    .catch((err) => res.status(404).json({ msg: "No Item found" }));
+});
+
+app.post("/item/add", (req, res) => {
+  const newItem = new Item({
+    name: req.body.name,
+    description: req.body.description,
+    quantity: req.body.quantity,
+  });
+
+  newItem
+    .save()
+    .then((item) => {
+      res.send(item);
+    })
+    .catch((err) => console.log(err.message));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
-})
+});
