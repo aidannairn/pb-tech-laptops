098f493d1182927600d9e81d6053e6ba77740c21
Adan Millard <adanmillard@gmail.com>
2022-09-13 17:17:55 +1200

    fixed docker compose files to containerize front and back end

--- server/index.js
+++ server/index.js
@@ -5,9 +5,6 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 
-// app.get("/", (req, res) => {
-//   res.send("Express + TypeScript Server");
-// });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
