f9c721f5693eca3e21fe38a9da7164d5dff5a986
aidannairn <aidan.nairn95@gmail.com>
2022-09-13 12:58:39 +1200

    feat: Add laptops and laptop root queries.

--- server/schema/laptopSchema.js
+++ server/schema/laptopSchema.js
@@ -22,3 +22,22 @@ const LaptopType = new GraphQLObjectType({
     images: { type: new GraphQLList(GraphQLString) }
   })
 })
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    laptops: {
+      type: new GraphQLList(LaptopType),
+      resolve(parent, args) {
+        return Laptop.find()
+      }
+    },
+    laptop: {
+      type: LaptopType,
+      args: { id: { type: GraphQLID }},
+      resolve(parent, args) {
+        return Laptop.findById(args.id)
+      }
+    }
+  }
+})
