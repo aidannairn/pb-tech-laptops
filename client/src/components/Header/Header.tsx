6ca7aaf302ffd3fe05f65b3dba15513f6c24d123
Nirmal Patel <patelnirmal17592@gmail.com>
2022-09-16 09:58:11 +1200

    display products to be added

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import style from "./Header.module.css";
 
 const Header: React.FC = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className={style.headerContainer}>
+      <div className={style.inputs}>
+        <input type="search"></input>
+        <button type="button">Search</button>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Header from '../components/Header/Header'
 
 const LandingPage: React.FC = () => {
   return (
-    <div>LandingPage</div>
+    <Header></Header>
   )
 }
 
