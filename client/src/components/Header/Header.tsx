4a81b07b799885a34afe0f7c5e38eec4b518d581
Nirmal Patel <patelnirmal17592@gmail.com>
2022-09-18 10:49:15 +1200

    header added

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,13 +1,28 @@
 import React from "react";
-import style from "./Header.module.css";
+import styles from "./Header.module.css";
 
 const Header: React.FC = () => {
   return (
-    <div className={style.headerContainer}>
-      <div className={style.inputs}>
+    <div className={styles.headerContainer}>
+      <div className={styles.headerBlackWrapper}>
+        <div className={styles.headerBlack}>
+          <div>PB Tech</div>
+          <div>Hardwired</div>
+          <div>PB Business</div>
+          <div>PB Education</div>
+          <div>PB Wholesale</div>
+          <div>PB Government</div>
+          <div>PB Health</div>
+          <div>PB IoT</div>
+        </div>
+      </div>
+      <div className={styles.headerBlueWrapper}>
+        
+      </div>
+      {/* <div className={styles.inputs}>
         <input type="search"></input>
         <button type="button">Search</button>
-      </div>
+      </div> */}
     </div>
   );
 };
