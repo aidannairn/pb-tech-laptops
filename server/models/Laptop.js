0111c8d8d77f54c78cb85af68f3d6dda966fcf01
aidannairn <aidan.nairn95@gmail.com>
2022-09-19 15:50:41 +1200

    feat: Add additional fields to Laptop model.

--- server/models/Laptop.js
+++ server/models/Laptop.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose')
 
 const LaptopSchema = new mongoose.Schema({
   name: { type: String },
+  caption: { type: String },
   types: [{ type: String }],
   quantity: { type: Number },
   price: { type: Number },
   images: [{ type: String }],
+  isTrending: { type: Boolean },
+  isOnSpecial: { type: Boolean },
+  amountSold: { type: Number },
+  userRatings: [{ type: Number }],
   bundleIDs: [{ 
     type: mongoose.Schema.Types.ObjectId,
     ref: 'LaptopBundle'
