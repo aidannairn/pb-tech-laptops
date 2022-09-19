27c1289e1ec739e076100b6c3a49005f2280b834
Adan Millard <adanmillard@gmail.com>
2022-09-19 13:51:07 +1200

    styling changes made, saving to merge main into branch

--- client/src/components/productHeader/ProductHeader.tsx
+++ client/src/components/productHeader/ProductHeader.tsx
@@ -1,22 +1,23 @@
 import laptopData from "./mockData";
+import "./productHeader.scss";
 
 export const ProductHeader: React.FC = () => {
   return (
-    <div>
-      <div>
+    <div className="PH-main-container">
+      <div className="PH-page-link">
         <h3>
           Placeholder Home {">"} computers laptops {">"} laptops {">"} business
           laptops
         </h3>
       </div>
-      <div>
+      <div className="PH-main-content">
         {laptopData.map((image, i) => {
           return (
-            <div className="left" key={i}>
-              <div className="large-laptop-img">
+            <div className="PH-left-container" key={i}>
+              <div className="PH-large-laptop-img">
                 <img src={image.mainImage} alt="laptop"></img>
               </div>
-              <div className="small-laptop-img">
+              <div className="PH-small-laptop-img">
                 <img src={image.mainImage} alt="laptop" />
                 <img src={image.subImageOne} alt="laptop" />
                 <img src={image.subImageTwo} alt="laptop" />
@@ -26,17 +27,17 @@ export const ProductHeader: React.FC = () => {
             </div>
           );
         })}
-        <div className="right">
+        <div className="PH-right-container">
           {laptopData.map((item, i) => {
             return (
-              <div className="information" key={i}>
-                <div>
-                  <h1>{item.laptopName}</h1>
-                  <h4>{item.subName}</h4>
+              <div className="PH-information" key={i}>
+                <div className="PH-title-container">
+                  <h1 className="PH-product-title">{item.laptopName}</h1>
+                  <h4 className="PH-product-desc">{item.subName}</h4>
                 </div>
-                <div>
-                  <p>{item.description}</p>
-                  <ul>
+                <div className="PH-information-container">
+                  <p className="PH-item-desc">{item.description}</p>
+                  <ul className="PH-UL">
                     <li>
                       <b>{item.subNameOne}</b>
                     </li>
@@ -51,13 +52,13 @@ export const ProductHeader: React.FC = () => {
               </div>
             );
           })}
-          <div className="reviews">
-            <div className="stars">
+          <div className="PH-reviews">
+            <div className="PH-stars">
               <span>Stars</span>
               <p>See all Reviews 25</p>
             </div>
           </div>
-          <div className="Shipping-info">
+          <div className="PH-shipping-info">
             <ul>
               <li>Ships Tomorrow</li>
               <li>Click {"&"} Collect available in 13 stores</li>
@@ -65,17 +66,17 @@ export const ProductHeader: React.FC = () => {
           </div>
           {laptopData.map((price, i) => {
             return (
-              <div className="price" key={i}>
+              <div className="PH-price" key={i}>
                 <h1>{price.price}</h1>
               </div>
             );
           })}
         </div>
-        <div className="buttons">
-          <button>Add to Cart</button>
-          <button>-</button>
+        <div className="PH-buttons">
+          <button className="PH-add-to-cart">Add to Cart</button>
+          <button className="minus-btn">-</button>
           <span>0</span>
-          <button>+</button>
+          <button className="plus-btn">+</button>
         </div>
       </div>
     </div>
