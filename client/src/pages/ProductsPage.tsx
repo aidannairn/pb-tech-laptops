392119018b66d312be786ba3a03f6e0582e67ef0
aidannairn <aidan.nairn95@gmail.com>
2022-09-21 07:57:40 +1200

    feat: Add ProductsPage on /products route.

---
+++ client/src/pages/ProductsPage.tsx
@@ -0,0 +1,15 @@
+import Banner from "../components/Banner/Banner"
+
+interface Props {
+
+}
+
+const ProductsPage: React.FC<Props> = () => {
+  return (
+    <div>
+      <Banner />
+    </div>
+  )
+}
+
+export default ProductsPage
