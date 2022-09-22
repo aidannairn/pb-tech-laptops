3a1e257cc09ab1401db226786eb124c015ed69f0
Nirmal Patel <patelnirmal17592@gmail.com>
2022-09-22 13:37:10 +1200

    not working

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,35 +1,40 @@
 import React, { useState } from "react";
 import styles from "./Carousel.module.css";
-import wish from '../../images/heart.png'
-import {useQuery} from '@apollo/client'
+import wish from "../../images/heart.png";
+import { useQuery } from "@apollo/client";
 import { GET_ALL_LAPTOPS } from "../../queries/laptopQueries";
-import Rating from '@mui/material/Rating';
+import Rating from "@mui/material/Rating";
 
 const Carousel: React.FC = () => {
-  
   interface Laptops {
-      name?: string
-      brand?: string
-      caption?: string
-      types?: [string]
-      price?: number
-      images?: string
-      isTrending?: boolean
-      isOnSpecial?: boolean
-      amountSold?: number
-      userRatings?: [number]
+    name: string;
+    brand: string;
+    caption: string;
+    types: [string];
+    price: number;
+    images: string;
+    isTrending: boolean;
+    isOnSpecial: boolean;
+    amountSold: number;
+    userRatings: [number];
   }
 
   interface Laptops {
     laptops: []
   }
 
-  const {error, loading, data} = useQuery<Laptops>(GET_ALL_LAPTOPS)
+  console.log(GET_ALL_LAPTOPS)
 
-  if(error){console.log(error)}
-  if(loading){console.log('loading...')}
+  const { error, loading, data } = useQuery<Laptops | any>(GET_ALL_LAPTOPS);
 
-  console.log(data?.laptops)
+  if (error) {
+    console.log(error);
+  }
+  if (loading) {
+    console.log("Loading...");
+  }
+
+  console.log(data);
 
   const [rightIndex, setRightIndex] = useState<number>(4);
   const [leftIndex, setLeftIndex] = useState<number>(0);
@@ -53,27 +58,34 @@ const Carousel: React.FC = () => {
           </div>
         )}
         <div className={styles.products}>
-          {data?.laptops.slice(leftIndex, rightIndex).map((value: any, i: number) => (
-            <div className={styles.card} key={i}>
-              <div className={styles.wish}><img src={wish} alt='wish'></img></div>
-              <div className={styles.image}>
-                <img
-                  src={value.images}
-                  alt="laptop"
-                ></img>
-              </div>
+          {data?.laptops
+            .slice(leftIndex, rightIndex)
+            .map((value: any, i: number) => (
+              <div className={styles.card} key={i}>
+                <div className={styles.wish}>
+                  <img src={wish} alt="wish"></img>
+                </div>
+                <div className={styles.image}>
+                  <img src={value.images} alt="laptop"></img>
+                </div>
 
-              <div className={styles.info}>
-                <h3>${value.price}</h3>
-                <p><Rating name="read-only" value={value.userRatings[0]} readOnly /></p>
-                <p>{value.name}</p>
-                <p>{value.caption}</p>
+                <div className={styles.info}>
+                  <h3>${value.price}</h3>
+                  <p>
+                    <Rating
+                      name="read-only"
+                      value={value.userRatings[0]}
+                      readOnly
+                    />
+                  </p>
+                  <p>{value.name}</p>
+                  <p>{value.caption}</p>
+                </div>
+                <div className={styles.button}>
+                  <button type="button">Add to cart</button>
+                </div>
               </div>
-              <div className={styles.button}>
-                <button type="button">Add to cart</button>
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
         {rightIndex <= data.laptops.length - 1 && (
           <div onClick={handleRight}>
